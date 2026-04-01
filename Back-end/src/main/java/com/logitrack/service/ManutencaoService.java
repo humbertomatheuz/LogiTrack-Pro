1010bd46a89acb9d1d050e59ea3935259ee1abed
humberto.matheus <hmsantana@stefanini.com>
2026-04-01 17:43:49 -0300

    map

--- Back-end/src/main/java/com/logitrack/service/ManutencaoService.java
+++ Back-end/src/main/java/com/logitrack/service/ManutencaoService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
