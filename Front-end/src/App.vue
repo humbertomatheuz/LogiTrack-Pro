6525926954d117a3249083819c704f09dad9eab0
humberto.matheus <hmsantana@stefanini.com>
2026-04-01 21:55:47 -0300

    keep alive render

--- Front-end/src/App.vue
+++ Front-end/src/App.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+const KEEP_ALIVE_INTERVAL = 5 * 60 * 1000 // 10 minutos
+let intervalId = null
+
+const keepAlive = async () => {
+  try {
+    const apiURL = import.meta.env.VITE_API_URL || ''
+    await fetch(apiURL, { mode: 'no-cors' })
+    console.debug('Keep-alive ping enviado com sucesso.')
+  } catch (error) {
+  }
+}
+
+onMounted(() => {
+  keepAlive()
+  
+  intervalId = setInterval(keepAlive, KEEP_ALIVE_INTERVAL)
+})
+
+onUnmounted(() => {
+  if (intervalId) clearInterval(intervalId)
+})
+</script>
+
 <template>
   <RouterView />
 </template>
